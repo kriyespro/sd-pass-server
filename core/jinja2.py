f605f2f04fa8a025e8a9925cbb7868578d9b1e4d
Kumar Sunil Verma <kumarsunilverma@gmail.com>
2026-05-26 22:52:17 +0530

    all images log-terminal22222

--- core/jinja2.py
+++ core/jinja2.py
@@ -1,6 +1,9 @@
+import json
+
 from django.contrib.staticfiles.storage import staticfiles_storage
 from django.urls import reverse
 from jinja2 import Environment
+from markupsafe import Markup
 
 
 def reverse_url(viewname, *args, **kwargs):
@@ -11,10 +14,21 @@ def reverse_url(viewname, *args, **kwargs):
     return reverse(viewname)
 
 
+def _tojson(value):
+    """JSON serialization safe for inline <script> tags (escapes </script> breakout)."""
+    return Markup(
+        json.dumps(value, ensure_ascii=False)
+        .replace('<', '\\u003c')
+        .replace('>', '\\u003e')
+        .replace('&', '\\u0026')
+    )
+
+
 def environment(**options):
     env = Environment(**options)
     env.globals.update(
         static=staticfiles_storage.url,
         url=reverse_url,
     )
+    env.filters['tojson'] = _tojson
     return env

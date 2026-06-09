210a6ffe900164462b9f4986a7df5d4b348550ac
Kumar Sunil Verma <kumarsunilverma@gmail.com>
2026-06-09 19:52:34 +0530

    partners & remove old affliate link

--- apps/affiliates/urls.py
+++ apps/affiliates/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path
+from django.views.generic import RedirectView
 
 from . import views
 
 app_name = 'affiliates'
 
 urlpatterns = [
-    path('', views.AffiliateHubView.as_view(), name='apply'),
-    path('dashboard/', views.AffiliateDashboardView.as_view(), name='dashboard'),
-    path('thanks/', views.affiliate_success, name='success'),
+    path('', RedirectView.as_view(pattern_name='affiliates:partner', permanent=False), name='apply'),
+    path('dashboard/', RedirectView.as_view(pattern_name='affiliates:partner', permanent=False), name='dashboard'),
+    path('thanks/', RedirectView.as_view(pattern_name='affiliates:partner', permanent=False), name='success'),
     path('partner/', views.PartnerPageView.as_view(), name='partner'),
 ]
